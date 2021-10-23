f90c69314200480c236f4871ea7364d11f1d2c6b
GakiyaYukimitsu <55187509+GakiyaYukimitsu@users.noreply.github.com>
2021-10-23 19:48:09 +0900

    fix

--- src/gakiya.jsx
+++ src/gakiya.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 const gakiya = () => {
     return (
         <div>
-            Hello
+            Hello---
         </div>
     );
 }
--- src/tmp.js
+++ src/tmp.js
@@ -1,9 +1,6 @@
 
 import React from 'react';
-
-//materialUIのボタンを使う
 import { Button,Box } from "@material-ui/core";
-
 import { Link } from 'react-router-dom';
 
 class page extends React.Component {
@@ -22,9 +19,19 @@ class page extends React.Component {
                 <Button variant="contained" color="primary" component={Link} to="/itasaka.jsx" >板坂</Button>
                 <Button variant="contained" color="primary" component={Link} to="/gakiya.jsx" >我喜屋</Button>
                 <Button variant="contained" color="primary" component={Link} to="/takahashi.jsx" >高橋</Button>
-                <br/>
             </Box>
             <p>開発後にこのページは削除想定</p>
+            <pre>{`
+            ・DEKIRUメイン画面★板坂
+                ⇨App.jsx板坂作業中
+            ・質問ページ★板坂※コンポーネント作成
+            ・Video通話★我喜屋※コンポーネント作成
+            ・質問一★板坂※コンポーネント作成
+            ・質問の詳細★高橋※コンポーネント作成
+                ⇨質問の詳表示
+                要素：カテゴリー、タイトル、詳細
+                ⇨回答できる人がオンラインかオフライン判断できる
+            `}</pre>
             </>
         );
     };
