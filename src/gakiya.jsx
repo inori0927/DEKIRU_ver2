21fb843161d5cb1bb0d08f7c313d4169e74b2613
GakiyaYukimitsu <55187509+GakiyaYukimitsu@users.noreply.github.com>
2021-11-07 19:59:45 +0900

    ログイン画面を追加

--- src/gakiya.jsx
+++ src/gakiya.jsx
@@ -1,10 +1,34 @@
+
 import React from 'react';
-const gakiya = () => {
-    return (
-        <div>
-            Hello---
-        </div>
-    );
+import { Button, Box } from "@material-ui/core";
+
+class gakiya extends React.Component {
+    render() {
+        return (
+            <>
+                <Box display="flex" justifyContent="space-between"
+                sx={{
+                    height: 100,
+                    display: "flex",
+                    padding: 8,
+                    justifyContent: "space-around",
+                    alignItems: "center"
+                }}
+            >
+                <div layout="columns">
+                <h1>ログイン</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <input placeholder="userid" />
+                    <input placeholder="password" />
+                    <div style={{ textAlign: "center" }}>
+                        <Button variant="contained" color="primary" type="submit">ログイン</Button>
+                    </div>
+                </form>
+                </div>
+            </Box>
+            </>
+        );
+    }
 }
 
 export default gakiya;
