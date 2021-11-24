b47b38d3ba9df19f9ddad9d7320f4bca51333967
ittetsu <ittetsusato@gmail.com>
2021-11-24 18:24:50 +0900

    skyway 相手とのテレビ通話実装

--- public/js/app.js
+++ public/js/app.js
@@ -17002,54 +17002,172 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
-/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
-/* harmony import */ var skyway_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! skyway-js */ "./node_modules/skyway-js/dist/skyway.js");
-/* harmony import */ var skyway_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(skyway_js__WEBPACK_IMPORTED_MODULE_1__);
-/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! react/jsx-runtime */ "./node_modules/react/jsx-runtime.js");
+/* harmony import */ var _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime/regenerator */ "./node_modules/@babel/runtime/regenerator/index.js");
+/* harmony import */ var _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var skyway_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! skyway-js */ "./node_modules/skyway-js/dist/skyway.js");
+/* harmony import */ var skyway_js__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(skyway_js__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! react/jsx-runtime */ "./node_modules/react/jsx-runtime.js");
+
+
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+function _iterableToArrayLimit(arr, i) { var _i = arr == null ? null : typeof Symbol !== "undefined" && arr[Symbol.iterator] || arr["@@iterator"]; if (_i == null) return; var _arr = []; var _n = true; var _d = false; var _s, _e; try { for (_i = _i.call(arr); !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 
 
 
-var peer = new (skyway_js__WEBPACK_IMPORTED_MODULE_1___default())({
-  key: 'c8164b05-9527-46f8-b756-3119c447a7cc'
+
+
+var peer = new (skyway_js__WEBPACK_IMPORTED_MODULE_2___default())({
+  key: '95ba327e-64d1-4c05-8f9f-ad00ac893e07'
 });
 
 function Skyway() {
-  //DOM要素の取得
-  var localVideo = (0,react__WEBPACK_IMPORTED_MODULE_0__.useRef)(null);
-  var localStream; // カメラ映像取得
+  var _useState = (0,react__WEBPACK_IMPORTED_MODULE_1__.useState)(''),
+      _useState2 = _slicedToArray(_useState, 2),
+      myId = _useState2[0],
+      setMyId = _useState2[1];
 
-  navigator.mediaDevices.getUserMedia({
-    video: true,
-    audio: true
-  }).then(function (stream) {
-    // 成功時にvideo要素にカメラ映像をセット
-    localVideo.current.srcObject = stream; // 着信時に相手にカメラ映像を返せるように、グローバル変数に保存しておく
+  var _useState3 = (0,react__WEBPACK_IMPORTED_MODULE_1__.useState)(''),
+      _useState4 = _slicedToArray(_useState3, 2),
+      callId = _useState4[0],
+      setCallId = _useState4[1]; //DOM要素の取得
 
-    localStream = stream;
-  })["catch"](function (error) {
-    // 失敗時にはエラーログを出力
-    console.error('mediaDevice.getUserMedia() error:', error);
-    return;
+
+  var localVideo = (0,react__WEBPACK_IMPORTED_MODULE_1__.useRef)(null);
+  var remoteVideo = (0,react__WEBPACK_IMPORTED_MODULE_1__.useRef)(null); //open: SkyWayサーバーとの接続が成功したタイミングで発火
+
+  peer.on('open', function () {
+    //PeerID取得
+    setMyId(peer.id); // カメラ映像取得
+
+    navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true
+    }).then(function (stream) {
+      // 成功時にvideo要素にカメラ映像をセット
+      localVideo.current.srcObject = stream;
+    })["catch"](function (error) {
+      // 失敗時にはエラーログを出力
+      console.error('mediaDevice.getUserMedia() error:', error);
+      return;
+    });
+  }); //call: 相手から接続要求が来たタイミングで発火
+
+  peer.on('call', function (mediaConnection) {
+    mediaConnection.answer(localVideo.current.srcObject); //相手の映像をvideo要素にセット
+
+    mediaConnection.on('stream', /*#__PURE__*/function () {
+      var _ref = _asyncToGenerator( /*#__PURE__*/_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default().mark(function _callee(stream) {
+        return _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default().wrap(function _callee$(_context) {
+          while (1) {
+            switch (_context.prev = _context.next) {
+              case 0:
+                remoteVideo.current.srcObject = stream;
+
+              case 1:
+              case "end":
+                return _context.stop();
+            }
+          }
+        }, _callee);
+      }));
+
+      return function (_x) {
+        return _ref.apply(this, arguments);
+      };
+    }());
+  }); //error: 何らかのエラーで発火
+
+  peer.on('error', function (err) {
+    alert(err.message);
+  }); //close: 接続が切れたときに発火
+
+  peer.on('close', function () {
+    alert('通信が切断しました。');
   });
-  return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)("div", {
+
+  var makeCall = function makeCall() {
+    //peer.call()で接続 => mediaConnectionに接続相手の情報が帰ってくる
+    var mediaConnection = peer.call(callId, localVideo.current.srcObject);
+    console.log('相手にコール'); //相手の映像をvideo要素にセット
+
+    mediaConnection.on('stream', /*#__PURE__*/function () {
+      var _ref2 = _asyncToGenerator( /*#__PURE__*/_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default().mark(function _callee2(stream) {
+        return _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default().wrap(function _callee2$(_context2) {
+          while (1) {
+            switch (_context2.prev = _context2.next) {
+              case 0:
+                remoteVideo.current.srcObject = stream;
+                _context2.next = 3;
+                return remoteVideo.current.play()["catch"](console.error);
+
+              case 3:
+              case "end":
+                return _context2.stop();
+            }
+          }
+        }, _callee2);
+      }));
+
+      return function (_x2) {
+        return _ref2.apply(this, arguments);
+      };
+    }());
+  };
+
+  return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("div", {
     className: "container",
-    children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)("div", {
+    children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("div", {
       className: "row justify-content-center",
-      children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)("div", {
+      children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("div", {
         className: "col-md-8",
-        children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)("div", {
+        children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)("div", {
           className: "card",
-          children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)("video", {
-            id: "my-video",
-            width: "400px",
-            ref: localVideo,
-            style: {
-              transform: 'scale(-1,1)'
-            },
-            autoPlay: true,
-            muted: true,
-            playsInline: true
-          })
+          children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("div", {
+            children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("video", {
+              width: "400px",
+              ref: localVideo,
+              style: {
+                transform: 'scale(-1,1)'
+              },
+              autoPlay: true,
+              muted: true,
+              playsInline: true
+            })
+          }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("div", {
+            children: myId
+          }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)("div", {
+            children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("input", {
+              value: callId,
+              onChange: function onChange(e) {
+                return setCallId(e.target.value);
+              }
+            }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("button", {
+              onClick: makeCall,
+              children: "\u767A\u4FE1"
+            })]
+          }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("div", {
+            children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("video", {
+              width: "400px",
+              ref: remoteVideo,
+              autoPlay: true,
+              muted: true,
+              playsInline: true
+            })
+          })]
         })
       })
     })
--- resources/js/pages/Skyway.js
+++ resources/js/pages/Skyway.js
@@ -1,38 +1,86 @@
 import React, {useState, useRef} from 'react';
 import Peer from 'skyway-js';
-const peer = new Peer({key: 'c8164b05-9527-46f8-b756-3119c447a7cc'});
+const peer = new Peer({key: '95ba327e-64d1-4c05-8f9f-ad00ac893e07'});
 
 
 function Skyway() {
+  const [myId, setMyId] = useState('');
+  const [callId, setCallId] = useState('');
   //DOM要素の取得
   const localVideo = useRef(null);
-  let localStream;
+  const remoteVideo = useRef(null);
 
-  // カメラ映像取得
-  navigator.mediaDevices.getUserMedia({video: true, audio: true})
-    .then( stream => {
-    // 成功時にvideo要素にカメラ映像をセット
-    localVideo.current.srcObject = stream;
-    // 着信時に相手にカメラ映像を返せるように、グローバル変数に保存しておく
-    localStream = stream;
-  }).catch( error => {
-    // 失敗時にはエラーログを出力
-    console.error('mediaDevice.getUserMedia() error:', error);
-    return;
+  //open: SkyWayサーバーとの接続が成功したタイミングで発火
+  peer.on('open', () => {
+    //PeerID取得
+    setMyId(peer.id);
+    // カメラ映像取得
+    navigator.mediaDevices.getUserMedia({video: true, audio: true})
+      .then( stream => {
+      // 成功時にvideo要素にカメラ映像をセット
+      localVideo.current.srcObject = stream;
+    }).catch( error => {
+      // 失敗時にはエラーログを出力
+      console.error('mediaDevice.getUserMedia() error:', error);
+      return;
+    });
   });
+
+  //call: 相手から接続要求が来たタイミングで発火
+  peer.on('call', mediaConnection => {
+    mediaConnection.answer(localVideo.current.srcObject);
+    //相手の映像をvideo要素にセット
+    mediaConnection.on('stream', async stream => {
+      remoteVideo.current.srcObject = stream;
+    });
+  });
+
+  //error: 何らかのエラーで発火
+  peer.on('error', err => {
+    alert(err.message);
+  });
+
+  //close: 接続が切れたときに発火
+  peer.on('close', () => {
+    alert('通信が切断しました。');
+  });
+
+  const makeCall = () => {
+    //peer.call()で接続 => mediaConnectionに接続相手の情報が帰ってくる
+    const mediaConnection = peer.call(callId, localVideo.current.srcObject);
+    console.log('相手にコール')
+    //相手の映像をvideo要素にセット
+    mediaConnection.on('stream', async stream => {
+      remoteVideo.current.srcObject = stream;
+      await remoteVideo.current.play().catch(console.error);
+    });
+  }
 
   return (
       <div className="container">
           <div className="row justify-content-center">
               <div className="col-md-8">
                   <div className="card">
+                      <div>
+                        <video
+                        width="400px"
+                        ref={localVideo}
+                        style={{transform: 'scale(-1,1)'}}
+                        autoPlay muted playsInline>
+                        </video>
+                      </div>
+                      <div>{myId}</div>
+                      <div>
+                        <input value={callId} onChange={e => setCallId(e.target.value)}></input>
+                        <button onClick={makeCall}>発信</button>
+                      </div>
+                      <div>
                       <video
-                      id="my-video"
                       width="400px"
-                      ref={localVideo}
-                      style={{transform: 'scale(-1,1)'}}
-                      autoPlay muted playsInline></video>
-                      {/* <Button color="primary" variant="contained" href={'/'}>HOMEに遷移ボタン</Button>  */}
+                      ref={remoteVideo}
+                      autoPlay muted playsInline>
+                      </video>
+                      </div>
                   </div>
               </div>
           </div>
