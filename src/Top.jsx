c1c404e95dbd8a602cc80aa92aa8e13e35a8506a
GakiyaYukimitsu <55187509+GakiyaYukimitsu@users.noreply.github.com>
2021-11-28 00:03:02 +0900

    通知画面、設定画面を追加

--- src/Top.jsx
+++ src/Top.jsx
@@ -1,18 +1,9 @@
-import logo from './logo.svg';
-
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './assets/styles/style.css'
-import { categoryList } from './componets';
-import { category } from './componets';
 import { Grid } from '@material-ui/core';
 import Header from './componets/Heder';
-import Content from './componets/QUES/Content';
-import Box from '@mui/material/Box';
 import TopBar from './componets/TopBar'
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import page from './tmp';
-import gakiya from './gakiya';
-import takahashi from './takahashi';
 import Copyright from './componets/Copyright';
 import login from './componets/login'
 import SignUp from './componets/SignUp';
@@ -21,6 +12,8 @@ import BasicDetail from './componets/USERS/BasicDetail';
 import Confirm from './componets/QUES/Confirm';
 import QuestionList from './componets/QuestionList';
 import Mypage from './componets/USERS/Mypage';
+import Notific from './componets/USERS/notific';
+import Setting from './componets/USERS/setting';
 import QuestionPage from './componets/QUES/QuestionPage';
 
 export default function Top() {
@@ -34,28 +27,30 @@ export default function Top() {
     return (
         <Grid container direction="column">
             <Header setValue={setValue} />
-            {value == "Mypage" ?
-                <Mypage />
-                :
-                <>
-                    <isValue />
-                    <BrowserRouter>
-                        <Switch>
-                            <Route exact path="/" component={login} />
-                            <Route exact path="/componets/SignUp" component={SignUp} />
-                            <Route exact path="/componets/TopBar" component={TopBar} />
-                            <Route exact path={"/componets/TopBar/QuettionPage"} component={QuestionPage} />
-                            <Route exact path={"/componets/TopBar/Question"} component={Basic} />
-                            <Route exact path={"/componets/TopBar/QuestionList"} component={QuestionList} />
-                            <Route exact path={"/componets/TopBar/Mypage"} component={Mypage} />
-                            <Route exact path={"/componets/TopBar/Question/Detail"} component={BasicDetail} />
-                            <Route exact path={"/componets/TopBar/Question/Detail/Confirm"} component={Confirm} />
-                        </Switch>
-                    </BrowserRouter>
-                </>
+            { value == "Mypage" ?
+            <Mypage />
+            : value == "notific" ?
+                <Notific />
+                : value == "setting" ?
+                    <Setting />
+                    : <>
+                        <isValue />
+                        <BrowserRouter>
+                            <Switch>
+                                <Route exact path="/" component={login} />
+                                <Route exact path="/componets/SignUp" component={SignUp} />
+                                <Route exact path="/componets/TopBar" component={TopBar} />
+                                <Route exact path={"/componets/TopBar/QuettionPage"} component={QuestionPage} />
+                                <Route exact path={"/componets/TopBar/Question"} component={Basic} />
+                                <Route exact path={"/componets/TopBar/QuestionList"} component={QuestionList} />
+                                <Route exact path={"/componets/TopBar/Mypage"} component={Mypage} />
+                                <Route exact path={"/componets/TopBar/Question/Detail"} component={BasicDetail} />
+                                <Route exact path={"/componets/TopBar/Question/Detail/Confirm"} component={Confirm} />
+                            </Switch>
+                        </BrowserRouter>
+                    </>
             }
             <Copyright sx={{ mt: 5 }} />
         </Grid>
-
     );
 }
---
+++ src/componets/USERS/setting.jsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export default function setting() {
+  return (
+    <div>
+      setting
+    </div>
+  );
+}
